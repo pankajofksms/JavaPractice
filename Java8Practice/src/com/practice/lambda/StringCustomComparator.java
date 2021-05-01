1f0f2854abf332515244efbd8431818013504bad
pankaj <pankaj@192.168.29.172>
2021-05-02 04:05:49 +0530

    Add

--- Java8Practice/src/com/practice/lambda/StringCustomComparator.java
+++ Java8Practice/src/com/practice/lambda/StringCustomComparator.java
@@ -6,9 +6,7 @@ public class StringCustomComparator {
 	
 	public static void main(String[] args) {
 		String[] words = {"Today", "is", "a", "sunny","day"};
-		
-		//Printing words based on length in increasing order. 
-		
+				
 		Arrays.sort(words, (s1,s2) -> Integer.compare(s1.length(), s2.length()));
 		
 		System.out.println(Arrays.toString(words));
