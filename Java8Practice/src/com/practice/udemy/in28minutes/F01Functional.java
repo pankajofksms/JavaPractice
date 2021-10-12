4f81271a6e3222acb3e79d2c086b35d2fae069b7
kumap24 <pankaj.kumar@in.pega.com>
2021-10-12 23:32:06 +0530

    Add even number logic

--- Java8Practice/src/com/practice/udemy/in28minutes/F01Functional.java
+++ Java8Practice/src/com/practice/udemy/in28minutes/F01Functional.java
@@ -4,7 +4,16 @@ import java.util.List;
 
 public class F01Functional {
     public static void main(String[] args) {
-        functionalList(List.of(3, 24, 5, 2, 4, 12, 6, 2, 12));
+        List<Integer> integers = List.of(3, 24, 5, 2, 4, 12, 6, 2, 12);
+        functionalList(integers);
+        System.out.println("---Even List----");
+        printEvenList(integers);
+    }
+
+    private static void printEvenList(List<Integer> integers) {
+        integers.stream()
+                .filter(num -> num % 2 == 0)
+                .forEach(System.out::println);
     }
 
     private static void functionalList(List<Integer> integers) {
